9a97acaa97675bb19b715551b44dca9a2d26ae49
hena <preethihena.d16@iiits.in>
2019-09-16 17:14:06 +0530

    Updates Curl Script

--- curl_script.sh
+++ curl_script.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 url='https://aqueous-meadow-97023.herokuapp.com'
 ifsc='ALLA0210636'
 token=`curl -s --request POST  --data '{"username":"preethi"}' $url/generate_token`
 token="${token%\"}"
 token="${token#\"}"
-echo "$token"
+echo "\nToken : $token\n"
+echo "Query 1: GET API to fetch a bank details,given branch IFSC code: $ifsc \n"
 output=`curl -s -H "Authorization: Bearer ${token}" --request GET $url/banks/$ifsc`
 
 echo $output
 
-output=`curl -s -H "Authorization: Bearer ${token}"  --request GET $url/branches/INDIAN%20OVERSEAS%20BANK/MUMBAI?limit=5"&"offset=0`
+echo "\nQuery 2: GET API to fetch all details of branches,given \
+bank name = INDIAN OVERSEAS BANK and a city = MUMBAI \n"
+output=`curl -s -H "Authorization: Bearer ${token}"  --request GET \
+$url/branches/INDIAN%20OVERSEAS%20BANK/MUMBAI?limit=5"&"offset=0`
+
 echo $output
