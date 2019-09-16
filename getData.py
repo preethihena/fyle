75a681469920aef220f65155eebf5e019cc41d41
hena <preethihena.d16@iiits.in>
2019-09-16 17:00:39 +0530

    query using views

--- getData.py
+++ getData.py
@@ -8,17 +8,14 @@ cur = conn.cursor()
 
 
 def get_IFSC(ifsc):
-	cur.execute("select * from branches where ifsc= %s",[ifsc])
+	cur.execute("select * from bank_branches where ifsc=%s",[ifsc])
 	ans=cur.fetchone()
-	cur.execute("select name from banks where id= %s",[ans[1]])
-	ans2=cur.fetchone()
-	return ans+ans2
+	return ans
+
 
 
 def get_Branches(city,bank,limit,offset):
-	cur.execute("select * from banks where name=%s",[bank])
-	ans=cur.fetchone()
-	cur.execute("select * from branches where bank_id=%s \
-				and city=%s limit %s offset %s",[ans[1],city,limit,offset])
+	cur.execute("select * from bank_branches where bank_name=%s and city=%s\
+	 			limit %s offset %s",[bank,city,limit,offset])
 	ans2=cur.fetchall()
 	return ans2
